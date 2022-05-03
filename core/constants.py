41656afd4381d4e24c61fe8bab514631c90e1cc0
nekstas <nek.stas.other@yandex.ru>
2022-05-03 13:44:33 +0300

    Добавлен URL для поиска организаций

--- core/constants.py
+++ core/constants.py
@@ -4,6 +4,7 @@ from core.vec import Vec
 # Яндекс.Карты
 MAP_API_SERVER = 'https://static-maps.yandex.ru/1.x/'
 GEOCODER_API_SERVER = "http://geocode-maps.yandex.ru/1.x/"
+SEARCH_API_SERVER = 'https://search-maps.yandex.ru/v1/'
 
 YM_TMP_FILENAME = 'ym-tmp.png'
 YM_IMG_SIZE = '600,450'
